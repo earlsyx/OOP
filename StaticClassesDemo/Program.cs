74a2a88253b506c660f4b60cf7ad5e998f93b94d
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-17 06:49:20 +0800

    Add project files.

---
+++ StaticClassesDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+//static, you get one copy for the entire application
+// meaning if we store data , get stored accrossed the entire application
+
+using StaticClassesDemo;
+
+SayHello();
+static void SayHello()
+{
+    Console.WriteLine("Hello!");
+}
+
+UserMessages.SayGoodMorning();  
---
+++ StaticClassesDemo/UserMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassesDemo
+{
+    class UserMessages
+    {
+        //access modifier
+       public static void SayGoodMorning()
+       {
+           Console.WriteLine("Good Morning!");
+       }
+    }
+}
