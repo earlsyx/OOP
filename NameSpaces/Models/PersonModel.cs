2a08e4a0070a4fa5139c08c7e27f1cfbcf30c084
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-20 17:41:58 +0800

    -- use namespaces to organize your application logically. - classes helps, -namespaces helps, - method helps orients. - well organized wood shop. - mean t to b read hby humans - east yto read udnstanbel sperate,

---
+++ NameSpaces/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpaces.Models
+{
+    public class PersonModel
+    {
+        //Namespace is the idea that we have fully qualified name for everything
+        // prjoects use fioldrs, simlilar, corect name space
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+
+
+    }
+}
---
+++ NameSpaces/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//easy way to organize our project in a way that makes sense
+// allows us to do some neat stuff, with how we organize or put things together.
+
+using FoundationInfoNameSpace;
+using NameSpaces.Models; // can be anythinh we want them to be
+
+List<PersonModel> peoplpe = new List<PersonModel>();//in collection, butch of items, also in colleciton.generic, also inside all system. <> indicate a generic.
+Console.WriteLine( );
+PersonModel person = new PersonModel();
+// why we never use full name, why? because we are living in the same project
+Caclulations.Add(4, 3);
+Console.ReadLine();
+
+// you can have more than one class with the same namespace
+// by default same name as project
+// you dont modify naemspace by default buy instead create folders.
+
+// use folder strucuter, create a strucurer that easily maps out sto sometihng
+// understnadable. // can use microsfot namespace, but dont actually od it.
+// folder structurs, easy to undestand where what 
