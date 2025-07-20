143365906caa1662daad877e74f12b248f19bfb1
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-20 15:58:21 +0800

    read-only - get only - cannot be change outside the fulladress when dotting or accessing members of a class structs or object.

---
+++ HMProperties/AddressModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMProperties
+{
+    public class AddressModel
+    {
+        //Create a class that has preiprties for street, address, city, state and zip code.
+        //Add FullAddress Property that is read-only.
+        public string Street { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        public string FullAddress
+        {
+            get 
+            {
+                return $"{Street} {Address} {City} {ZipCode}"; 
+            }
+        }
+
+    }
+}
---
+++ HMProperties/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+//Create a class that has preiprties for street, address, city, state and zip code.
+//Add FullAddress Property that is read-only.
+
+using HMProperties;
+
+//dot operator (.) .. used to access to access membemrs 
+// properties, fields, methods etc. of a classs, a strucr or object.
+//fullAddress is an object, AddressModel is a type of that object and .ZipCode means accessing thembmers of fullAddress object
+AddressModel fullAddress = new AddressModel();
+fullAddress.Street = "Cherry Dv St";
+fullAddress.Address = "Blk 1 Lot 3";
+fullAddress.City = "Taytay Rizal";
+fullAddress.ZipCode = 1920;
+
+Console.WriteLine(fullAddress.FullAddress);
