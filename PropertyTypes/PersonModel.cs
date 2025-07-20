4cee8ee3c429cc311fd84790410b86b46990881b
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-20 15:39:33 +0800

    In calsses, use auto proeprties by default. MOVE TO full properties when needed. - constructor calling not actual class name when instantitating a class. () - use sfconstrcot to set value. - class that will hold data. - expand get set to allow protecting info. - auto propertiy prop tab tab - removing backing field and set inside the proprty entried - useful for priniting only or reading only. - set makesure data comign in is good data. -- assigning a value inside et, keword is value. - substring, return a certain part of a string based on the position speficy first position and continues until the end of string. - glimps of method overloading  - same method name , passed in different parms, treat as differnet metods. // constctor, sspecial type of method that get runs when you build your house or when you instantiate a class. can have params early.

---
+++ PropertyTypes/PersonModel.cs
@@ -0,0 +1,91 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTypes
+{
+    public class PersonModel
+    {
+        // class will hold data.
+        // expand get set, to allow protecting info.
+
+        //Auto Property
+        public string FirstName { private get; set; }
+        public string LastName { get; private set; }
+
+        private string _Password;
+
+        public string Password
+        {
+            set { _Password = value; }
+        }
+         
+        public string FullName 
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        //public int Age { get; set; } // negative , below zero not valid age. ,, upto what? 100?
+        //Full Property
+        private int _age; // private backing field, _ then camel case, don't manipualte directly,
+        // only inside age prop
+
+        public int Age
+        {
+            get
+            { 
+                return _age; //returning value
+            }
+            set 
+            { 
+                //making sure data coming in is good data.
+                if(value >= 0 && value < 126)
+                {
+                    _age = value; //assigining value, special keyword. value.
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age needs to be in a valid range");
+                }
+            }
+        }
+
+        //public string SSN { get; set; }
+        private string _ssn;
+
+        public string SSN 
+        {
+            get 
+            {
+                //123-45-6789 - 11 - 5 = 6
+                // masking ata, limit data.
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+                return output; 
+            }
+            set { _ssn = value; }
+        }
+
+
+        //Method overloading
+        // same method name, passed in different paramters, and this will be treat as different methods.
+
+        public PersonModel()
+        {
+            
+        }
+        // special type of method, constructor.
+
+        public PersonModel(string lastName)
+        {
+            LastName = lastName;
+             ///specital type of method that gets run when you build yourhouse
+        }
+
+
+    }
+}
---
+++ PropertyTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using PropertyTypes;
+
+PersonModel person = new PersonModel("Correy"); // not actuall the class you are calling but the constsructor (), normally ihdden.
+//use constcutor to set that value
+// make sure you have atleast the minimum amount of data to start out with.
+// a person has to have a firs tname last name before we declare them a person
+// i know i always have them, then just passing all params i the constrcutor, 
+// pass an assignemnt right away.
+person.FirstName = "Tim";
+//person.LastName = "Correy";
+person.Age = 40;
+person.SSN = "123-45-6789";
+
+Console.WriteLine(person.FullName );
+Console.WriteLine(person.SSN);
+Console.ReadLine();
